87a7759d751122bf7fe646e60e57aecd42a26f26
AtharvaG04 <153016742+AtharvaG04@users.noreply.github.com>
2025-07-31 14:08:48 +0530

    Update AuthContext.jsx
    replace local url to backend

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -5,7 +5,7 @@ axios.defaults.withCredentials = true;
 
 const AuthContext = createContext(null);
 
-const API_URL = 'http://localhost:8000/apis/lost-and-found/auth';
+const API_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const AuthProvider = ({ children }) => {
   const [authState, setAuthState] = useState('loading'); 
