88b1a15b292cbf35a0a5a8c36c0d493ff93670a5
AtharvaG04 <153016742+AtharvaG04@users.noreply.github.com>
2025-08-01 11:35:38 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ const io = new Server(server, {
 app.set('socketio', io);
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://khogaya-n-milgya-jv4yb6yt9-atharva-gujarthis-projects.vercel.app/',
     methods: ['GET', 'POST', 'OPTIONS', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
